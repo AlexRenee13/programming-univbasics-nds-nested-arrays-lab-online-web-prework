0d1d29d734af8932513d0ebcabf1bba0c569bd21
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-29 07:42:42 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,7 +31,9 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  
+  ENNET_HOUSE[2] = "Tiny Ewell"
+
+return ENNET_HOUSE
   
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
